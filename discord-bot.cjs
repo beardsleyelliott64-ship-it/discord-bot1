c8dfb6e01bbbc74b6c71715ff6a38fcee65daf5b
beardsleyelliott64-ship-it <beardsleyelliott64@gmail.com>
2026-08-21 00:34:23 +0100

    Add files via upload

---
+++ discord-bot.cjs
@@ -0,0 +1,15 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Bot logged in successfully as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
